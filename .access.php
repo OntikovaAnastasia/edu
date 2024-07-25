8e9f6986ac2a0d1347afbcd16fa446d67e448b8f
Anastasia Ontikova <ontikova2000@mail.ru>
2024-07-25 16:18:26 +0300

    task170213

--- .access.php
+++ .access.php
@@ -1,10 +1,9 @@
 <?
+$PERM["lichnyy-kabinet-pokupatelya"]["*"]="D";
+$PERM["lichnyy-kabinet-pokupatelya"]["G6"]="X";
+$PERM["lichnyy-kabinet-prodavtsa"]["*"]="D";
+$PERM["lichnyy-kabinet-prodavtsa"]["G7"]="X";
 $PERM["search"]["5"]="W";
-$PERM["products"]["5"]="W";
-$PERM["services"]["5"]="W";
-$PERM["news"]["5"]="W";
-$PERM["contacts"]["5"]="W";
-$PERM["company"]["5"]="W";
 $PERM["index.php"]["5"]="W";
 $PERM["/"]["*"]="R";
 ?>
